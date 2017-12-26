1de030d13276e30fb98dac29511af1d93387168f
北尾恵太 <kitaokeita@kitaokeita-no-MacBook-Air.local>
2017-12-26 19:51:46 +0900

    kkk

--- db/migrate/20171128211606_remove_useridticketid_from_reservation.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUseridticketidFromReservation < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :reservations, :user_id, foreign_key: true
-    remove_reference :reservations, :ticket_id, foreign_key: true
-  end
-end
