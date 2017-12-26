42613d641c39d7dcb60672f65d2b79a01e5286f0
北尾恵太 <kitaokeita@kitaokeita-no-MacBook-Air.local>
2017-12-26 19:40:50 +0900

    reservation model change

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -61,6 +61,6 @@ class ReservationsController < ApplicationController
 	private
 
 	def reservation_params
-		 params.require(:reservation).permit(:startdate,:enddate,:ticket_id, :approval)
+		 params.require(:reservation).permit(:startdate,:enddate,:ticket_id, :approval, :user_id)
 	end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,5 +3,4 @@ class Reservation < ApplicationRecord
  belongs_to :user
  belongs_to :ticket
 	
-	default_scope -> { order(created_at: :desc) }
 end
